ef2fc7b28f3b4ae8705faa062af8f356628c315c
marrtet <168977302+marrtet@users.noreply.github.com>
2024-05-28 00:36:33 +0300

    worked with repo structure and added ml into streamlit

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from src.model.model_fit_estimate import CianPredictionModel
+from src.preprocessing.cleaner.raw_data_process import DataProcess
+from src.preprocessing.scraper.cian_scraper import Scraper
+
+__all__ = ["CianPredictionModel", "Scraper", "DataProcess"]
